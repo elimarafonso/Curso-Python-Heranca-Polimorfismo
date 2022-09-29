51a4f969c3bdc9b930e3a9e35592e748d695a207
Elimar Afonso <elimar_afonso@hotmail.com>
2022-09-29 01:46:28 -0300

    menu jogos

---
+++ Introdução ao Python/jogos/forca.py
@@ -0,0 +1,11 @@
+def jogar():
+
+    print("*********************************")
+    print("***Bem vindo a jogo de Forca!****")
+    print("*********************************")
+
+    print("fim do jogo de Forca.")
+
+
+if(  __name__ == "__main__"):
+    jogar()
