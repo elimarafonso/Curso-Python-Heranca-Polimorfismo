fc40f83f28f3c9a25291bddd12844b9d0fc34314
Elimar Afonso <elimar_afonso@hotmail.com>
2022-10-04 02:14:36 -0300

    JOGO DA FORCA IMPLEMENTADO

--- Introdução ao Python/jogos/forca.py
+++ Introdução ao Python/jogos/forca.py
@@ -1,11 +1,47 @@
 def jogar():
+    print('*****************************************')
+    print('****** Bem-vindo ao jogo da Forca *******')
+    print('*****************************************')
 
-    print("*********************************")
-    print("***Bem vindo a jogo de Forca!****")
-    print("*********************************")
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    enforcou = False
+    acertou = False
+    erros = 0
 
-    print("fim do jogo de Forca.")
+    print(letras_acertadas)
+    while (not acertou and not enforcou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip().upper()
+
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = chute
+                index += 1
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6 - erros))
+
+        if erros == 6:
+            break
+        elif "_" not in letras_acertadas:
+            break
+
+        print(letras_acertadas)
+        print("Jogando...")
 
 
-if(  __name__ == "__main__"):
+
+    if "_" not in letras_acertadas:
+        print("VOCÊ VENCEU !!!")
+    else:
+        print("ENFORCADO !!! ")
+
+    print("Fim do jogo")
+
+
+if (__name__ == '__main__'):
     jogar()
